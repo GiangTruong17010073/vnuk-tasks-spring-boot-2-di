a70f0010899c272c22e442bf22c799ee8a296d33
Modern Coding <cormorant.programming@gmail.com>
2019-04-28 18:34:31 +0700

    No more filter

--- src/main/java/vn/edu/vnuk/tasks/VnukTasksSpringBoot2DiApplication.java
+++ src/main/java/vn/edu/vnuk/tasks/VnukTasksSpringBoot2DiApplication.java
@@ -2,27 +2,12 @@ package vn.edu.vnuk.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import vn.edu.vnuk.tasks.filter.ConnectionFilter;
 
 @SpringBootApplication
 public class VnukTasksSpringBoot2DiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VnukTasksSpringBoot2DiApplication.class, args);
-	}
-	
-	
-	@Bean
-	public FilterRegistrationBean<ConnectionFilter> connectionFilter() {
-	    FilterRegistrationBean<ConnectionFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-	    filterRegistrationBean.setFilter(new ConnectionFilter());
-	    filterRegistrationBean.setOrder(2);
-	    filterRegistrationBean.addUrlPatterns("/tasks");
-	    filterRegistrationBean.addUrlPatterns("/tasks/*");
-	    return filterRegistrationBean;
 	}
 
 }
